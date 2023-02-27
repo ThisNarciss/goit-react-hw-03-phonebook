402ac31843d7469ffc215a2cce2215a744432642
ThisNarciss <tyomaxxx000@gmail.com>
2023-02-27 16:58:33 +0200

    add proptypes

--- src/components/Section/Section.jsx
+++ src/components/Section/Section.jsx
@@ -5,6 +5,7 @@ import { SectionBox, Title, TitleBox, IconPhone } from './Section.styled';
 export class Section extends Component {
   static propTypes = {
     title: PropTypes.string.isRequired,
+    children: PropTypes.node,
   };
 
   render() {
@@ -13,10 +14,8 @@ export class Section extends Component {
       <SectionBox>
         <TitleBox>
           {title.toLowerCase() === 'phonebook' && <IconPhone size={30} />}
-
           <Title>{title}</Title>
         </TitleBox>
-
         {children}
       </SectionBox>
     );
